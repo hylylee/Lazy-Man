e7b2134828933f9b7f8f800c66dcd65491bf6139
CarlosChen <carlos.chen@oocl.com>
2019-07-24 08:44:48 +0800

    init project

---
+++ src/test/java/com/carlos/LAZY_MAN/LazyManApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carlos.LAZY_MAN;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LazyManApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
